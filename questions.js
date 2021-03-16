a0b6c123509fa8acadc846c92e27beaf5f648bd2
TMkeerthipriya <62497241+TMkeerthipriya@users.noreply.github.com>
2021-03-16 09:42:58 +0530

    Quiz

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a storage class?",
+    answer: "int",
+    options: [
+      "Static",
+      "Extern",
+      "Register",
+      "int"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following functions can be used to increase the size of dynamically allocated array?",
+    answer: "realloc()",
+    options: [
+      "realloc()",
+      "calloc()",
+      "malloc()",
+      "memadjust()"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Pointers can be used to achieve?",
+    answer: "call by reference",
+    options: [
+      "call by procedure",
+      "call by name",
+      "call by function",
+      "call by reference"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How much memory is required to store a value of type double?",
+    answer: "8 bytes",
+    options: [
+      "4 bytes",
+      "6 bytes",
+      "8 bytes",
+      "10 bytes"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the default return type of a function?",
+    answer: "Int",
+    options: [
+      "Void",
+      "Int",
+      "Char",
+      "Float"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
